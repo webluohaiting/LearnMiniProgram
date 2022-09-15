71702627ded171a924ab888e313882d6ee944e50
luohaiting <luohaiting@37.com>
2022-09-15 12:01:16 +0800

    03_小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
 // home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    name: 'haha',
+    students: [
+      { id: 1, name: 'aoao', age: 20 },
+      { id: 2, name: 'ouou', age: 30 },
+      { id: 3, name: 'xixi', age: 40 },
+      { id: 4, name: 'hehe', age: 50 },
+    ],
+    counter: 0
+  },
+  handleBtnClick() {
+    // 1.错误做法：只会改变值，不会改变视图
+    // this.data.counter += 1
+    // console.log(this.data.counter);
 
+    // 2.setData绑定数据
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  handeSubstaction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
